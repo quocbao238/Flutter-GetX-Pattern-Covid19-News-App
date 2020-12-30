fe2d6d5cdd661e5be24dc64e40517d12504450fc
Bui Quoc Bao <51372227+quocbao238@users.noreply.github.com>
2020-12-30 16:14:18 +0700

    Update home_controller.dart

--- lib/pages/home/controller/home_controller.dart
+++ lib/pages/home/controller/home_controller.dart
@@ -8,7 +8,7 @@ import 'package:getXDemo/widget/web_view_widget.dart';
 import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
 
 class HomeController extends GetxController {
-  // Khai báo biến
+  
   final lstNews = <NewsModel>[].obs;
   final lstWorldCovid = <WorldCovid>[].obs;
   final lstCovidTotal = <CovidTotalModel>[].obs;
